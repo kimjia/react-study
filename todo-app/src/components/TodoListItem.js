5ce7b311ef7057a3dfba28fafda24dc02451211a
kimjia <kimjia@snu.ac.kr>
2022-01-24 15:45:34 +0900

    optimize rendering

--- todo-app/src/components/TodoListItem.js
+++ todo-app/src/components/TodoListItem.js
@@ -1,20 +1,26 @@
 import { MdRadioButtonUnchecked, MdCheckCircle, MdClear } from 'react-icons/md';
 import './TodoListItem.scss';
 import cn from 'classnames';
+import React from 'react';
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
   const { id, text, checked } = todo;
   return (
-    <div className="TodoListItem">
-      <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
-        {checked ? <MdCheckCircle /> : <MdRadioButtonUnchecked />}
-        <div className="text">{text}</div>
-      </div>
-      <div className="remove" onClick={() => onRemove(id)}>
-        <MdClear />
+    <div className="TodoListItem-virtualized" style={style}>
+      <div className="TodoListItem">
+        <div
+          className={cn('checkbox', { checked })}
+          onClick={() => onToggle(id)}
+        >
+          {checked ? <MdCheckCircle /> : <MdRadioButtonUnchecked />}
+          <div className="text">{text}</div>
+        </div>
+        <div className="remove" onClick={() => onRemove(id)}>
+          <MdClear />
+        </div>
       </div>
     </div>
   );
 };
 
-export default TodoListItem;
+export default React.memo(TodoListItem);
