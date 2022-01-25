8805692fa74eea26fa04d945b8a3d95fd35bea54
kimjia <kimjia@snu.ac.kr>
2022-01-25 21:02:38 +0900

    context API tutorial

---
+++ 15/context-tutorial/src/components/SelectColors.js
@@ -0,0 +1,44 @@
+import { Component } from "react/cjs/react.production.min";
+import ColorContext from "../contexts/color";
+
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+class SelectColors extends Component {
+  static contextType = ColorContext;
+
+  handleSetColor = (color) => {
+    this.context.actions.setColor(color);
+  };
+
+  handleSetSubcolor = (color) => {
+    this.context.actions.setSubcolor(color);
+  };
+  render() {
+    return (
+      <div>
+        <h2>색상을 선택하세요.</h2>
+        <div style={{ display: "flex" }}>
+          {colors.map((color) => (
+            <div
+              key={color}
+              style={{
+                background: color,
+                width: "24px",
+                height: "24px",
+                cursor: "pointer",
+              }}
+              onClick={() => this.handleSetColor(color)}
+              onContextMenu={(e) => {
+                e.preventDefault();
+                this.handleSetSubcolor(color);
+              }}
+            />
+          ))}
+        </div>
+        <hr />
+      </div>
+    );
+  }
+}
+
+export default SelectColors;
